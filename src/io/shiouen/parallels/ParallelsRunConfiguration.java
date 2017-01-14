848cf2792668cdf0b1048fcbf50fa6fd2f25a9ba
Olivier Van Aken <olivier.van.aken@capgemini.com>
2017-01-15 00:36:36 +0100

    Started using ParallelsSettings model

--- src/io/shiouen/parallels/ParallelsRunConfiguration.java
+++ src/io/shiouen/parallels/ParallelsRunConfiguration.java
@@ -9,29 +9,39 @@ import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Set;
-
 import static io.shiouen.parallels.properties.Properties.from;
 
 public class ParallelsRunConfiguration extends RunConfigurationBase {
     private static final String properties = "/properties/run-config.properties";
     private static final String name = "name";
 
-    private Set<RunConfiguration> configurations;
+    private ParallelsSettings settings;
 
     public ParallelsRunConfiguration(Project project, @NotNull ParallelsConfigurationType type) {
         super(project, type.getConfigurationFactories()[0], from(properties).get(name));
+
+        this.settings = new ParallelsSettings();
+    }
+
+    public ParallelsSettings getSettings() {
+        return this.settings;
+    }
+
+    public void setSettings(ParallelsSettings settings) {
+        this.settings = settings;
     }
 
     @NotNull
     @Override
     public SettingsEditor<? extends RunConfiguration> getConfigurationEditor() {
-        return new ParallelsSettingsEditor();
+        return new ParallelsSettingsEditor(this.settings);
     }
 
     @Override
     public void checkConfiguration() throws RuntimeConfigurationException {
-
+        if (this.settings.hasRunConfigurations()) {
+            throw new RuntimeConfigurationError("No configurations selected.");
+        }
     }
 
     @Nullable
--- src/io/shiouen/parallels/ParallelsSettingsEditor.java
+++ src/io/shiouen/parallels/ParallelsSettingsEditor.java
@@ -1,25 +1,48 @@
 package io.shiouen.parallels;
 
+import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
+import io.shiouen.parallels.ui.JParallelsPanel;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.util.Set;
 
 public class ParallelsSettingsEditor extends SettingsEditor<ParallelsRunConfiguration> {
+    private ParallelsSettings settings;
+
+    private JParallelsPanel panel;
+
+    public ParallelsSettingsEditor() {
+        this(new ParallelsSettings());
+    }
+
+    public ParallelsSettingsEditor(ParallelsSettings settings) {
+        this.settings = settings;
+
+        Set<RunConfiguration> configurations = settings.getRunConfigurations();
+
+        for (RunConfiguration configuration : configurations) {
+
+        }
+
+        this.panel = new JParallelsPanel();
+    }
+
     @Override
     protected void resetEditorFrom(@NotNull ParallelsRunConfiguration parallelsRunConfiguration) {
-
+        this.settings = parallelsRunConfiguration.getSettings();
     }
 
     @Override
     protected void applyEditorTo(@NotNull ParallelsRunConfiguration parallelsRunConfiguration) throws ConfigurationException {
-
+        parallelsRunConfiguration.setSettings(this.settings);
     }
 
     @NotNull
     @Override
     protected JComponent createEditor() {
-        return null;
+        return this.panel;
     }
 }
